bfac2bbb4e7d16d4b05da25dc00845d04aa316cf
annahlv <annahlv.work@gmail.com>
2021-03-11 22:15:03 +0200

    feat: add algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let revert = '';
+  n = '' + Math.abs(n);
+  console.log(n);
+  for (i = 0; i <= n.length; i++) { 
+    revert += n.charAt(n.length - i);
+  }
+  return revert;
 }
